ec06d3aa05c5825fb2d1cac5fb3553d8e1995ab8
livbogdan <livbogdan@gmail.com>
2023-01-27 14:08:41 +0100

    Added Comments Changed colors

--- src/screens/RecipeDetails/index.js
+++ src/screens/RecipeDetails/index.js
@@ -4,13 +4,13 @@ import {Image, SafeAreaView, Text, View, ScrollView} from "react-native";
 import Title from "../../components/Title";
 import styles from "./Styles";
 
-const RecipeDetails = ({ route }) => {
+const RecipeDetails = ({ route }) => { 
     const { item } = route?.params || {};
     console.log('item?.instructions :>> ', item?.instructions);
-    const instructions = item?.instructions || [];
-    const nutrition = item?.nutrition;
-    delete nutrition?.updated_at;
-    const nutritionKeys = Object.keys(nutrition || {});
+    const instructions = item?.instructions || []; // Recipe step by step instruction
+    const nutrition = item?.nutrition; // row of recipe components
+    delete nutrition?.updated_at; // Remove from components when recipe updated
+    const nutritionKeys = Object.keys(nutrition || {}); 
 
     return (
         <SafeAreaView style={styles.container}>
@@ -23,21 +23,22 @@ const RecipeDetails = ({ route }) => {
                     <View key={key} style={styles.row}>
                         <Text style={styles.key}>{key}</Text>
                         <Text style={styles.value}>{nutrition[key]}</Text>
-                    </View>
+                    </View> 
                 ))}
 
-                <Title style={{ marginTop: 32, marginBottom: 16 }} text="Instructions" />
+                <Title style={{ marginTop: 32, marginBottom: 16 }} text="Instructions" />  
 
                 {instructions?.map((instruction, index) => (
                     <View key={instruction?.id} style={styles.instructionRow}>
-                        <Text style={styles.index}>{index + 1}</Text>
+                        <Text style={styles.index}>{index + 1}</Text> 
                         <Text style={styles.instructionText}>{instruction?.display_text}</Text>
                     </View>
                 ))}
 
-                {!instructions?.length ? (
-                    <Text style={styles.value}>No instructions found.</Text>
-                ) : null}
+                
+                {!instructions?.length ? (                                      // If instruction non available.
+                    <Text style={styles.value}>No instructions found.</Text> 
+                ) : null} 
 
             </ScrollView>
         </SafeAreaView>
